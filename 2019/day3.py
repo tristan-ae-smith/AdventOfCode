8e0d7667fccf79de5a2bdd2489f5d220f52d0412
Thomas Smith <tellisdon@googlemail.com>
2019-12-14 12:00:11 +0000

    Completed day 3 part 2.

--- 2019/day3.py
+++ 2019/day3.py
@@ -16,18 +16,22 @@ test2 = [["R98","U47","R26","D63","R33","U87","L62","D20","R33","U53","R51"],
 
 def writeWire(steps):
 	location = (0,0)	# initial location
-
+	step = 0
 	grid[location] = True
 	for segment in steps:
 		dir = dirs[segment[0]] # grab the relevant transform
 		dist = int(segment[1:])
 		for x in range(dist):
+			step += 1
 			location = tuple(map(sum,zip(location, dir)))	# quick-n-dirty coordinate addition: location + transform
-			grid[location] = True
+			grid[location] = step
 
 
 def mDist(loc): # manhattan distance
 	return abs(loc[0]) + abs(loc[1])
+
+def wDist(loc, dist): # manhattan distance
+	return (grid[loc] + dist) if loc in grid else sys.maxsize
 
 def readWire(steps):
 	location = (0,0)	# initial location
@@ -42,12 +46,41 @@ def readWire(steps):
 				cross = location if mDist(location) < mDist(cross) else cross
 	return cross
 
+def readWire2(steps):
+	location = (0,0)	# initial location
+	sumW = sys.maxsize
+
+	step = 0
+
+	for segment in steps:
+		dir = dirs[segment[0]] # grab the relevant transform
+		dist = int(segment[1:])
+		for x in range(dist):
+			# print(step)
+			step += 1
+			location = tuple(map(sum,zip(location, dir)))	# quick-n-dirty coordinate addition: location + transform
+			if location in grid:
+				# print("potential intersection found at ", grid[location], "+", step)
+				sumW = wDist(location, step) if wDist(location, step) < sumW else sumW
+	return sumW
+
 # writeWire(test2[0])
 # print(mDist(readWire(test2[1])))
 
-with open('day3.dat') as wInput:	# open directions file
-	wires = wInput.readlines()	# read directions
-	wires = [wire.strip().split(',') for wire in wires]
-	print(wires)
-	writeWire(wires[0])
-	print(mDist(readWire(wires[1])))
+def part1():
+	with open('day3.dat') as wInput:	# open directions file
+		wires = wInput.readlines()	# read directions
+		wires = [wire.strip().split(',') for wire in wires]
+		# print(wires)
+		writeWire(wires[0])
+		print(mDist(readWire(wires[1])))
+
+def part2():
+	with open('day3.dat') as wInput:	# open directions file
+		wires = wInput.readlines()	# read directions
+		wires = [wire.strip().split(',') for wire in wires]
+		writeWire(wires[0])
+		# print(grid)
+		print(readWire2(wires[1]))
+
+part2()
